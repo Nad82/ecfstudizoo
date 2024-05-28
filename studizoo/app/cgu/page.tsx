689273844df70f4802097c7c4e90b554c511afc1
Nadege Mbaka <mbakanad@hotmail.com>
2024-05-28 18:23:15 +0200

    pages

---
+++ studizoo/app/cgu/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function cgu() {
+    return (
+        <div>
+        cgu
+        </div>
+    )
+}
---
+++ studizoo/app/rgbd/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function rgbd() {
+    return (
+        <div>rgbd</div>
+    )
+}
