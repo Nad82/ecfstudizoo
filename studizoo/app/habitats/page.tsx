9c16085e0484d607ac1a865a50a46b1f8fef8090
Nadege Mbaka <mbakanad@hotmail.com>
2024-07-05 08:49:49 +0200

    pageHabitat

--- studizoo/app/habitats/page.tsx
+++ studizoo/app/habitats/page.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Habitat() {
     return (
-        <div>habitat</div>
+        <div className='bg-green-800'>
+            <div className='text-4xl text-yellow-400 text-center'>Les habitats</div>
+            <br />
+            <div className='text-2xl text-yellow-400'>Découvrez les habitats du zoo Arcadia</div>
+            <br />
+        </div>
     )
 }
